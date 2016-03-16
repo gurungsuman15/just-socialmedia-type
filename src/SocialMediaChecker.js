86840edcbf88c33e28bbcc9b6bea42670f511833
Kabir <kabirbaidhya@gmail.com>
2016-03-16 21:21:22 +0545

    Add stuffs

--- src/SocialMediaChecker.js
+++ src/SocialMediaChecker.js
@@ -1,24 +1,27 @@
 
- var patterns = {
+ const patterns = {
     'facebook': '^(https?\:\/\/)?(www\.)?(facebook|fb)\.[a-z]{2,3}(\/.+)+\/?$',
     'instagram': '^(https?\:\/\/)?(www\.)?(instagram)\.[a-z]{2,3}(\/.+)+\/?$',
     'linkedin': '^(https?\:\/\/)?(www\.)?(linkedin)\.[a-z]{2,3}(\/.+)+\/?$',
     'google+': '^(https?\:\/\/)?(www\.)?(plus.google)\.[a-z]{2,3}(\/.+)+\/?$'
 };
 
-
 class SocialMediaChecker {
-	check(url) {
+	static check(url) {
+        if (typeof url !== 'string') {
+            throw new Error('SocialMediaChecker.check() expects a string value.');
+        }
+
         for (let key in patterns) {
             let regex = new RegExp(patterns[key], 'i');
-            
+
             if (url.match(regex)) {
                 return key;
             }
         }
 
         return false;
-    }	
+    }
 }
 
 export default SocialMediaChecker;
--- tests/SocialMediaChecker.spec.js
+++ tests/SocialMediaChecker.spec.js
@@ -1,36 +1,34 @@
 
 import SocialMediaChecker from '../src/SocialMediaChecker';
 
-var checker = new SocialMediaChecker();
-
 describe('Facebook', () => {
 
     it('returns "facebook" for facebook url', () => {
-        var result = checker.check('https://www.facebook.com/john');
+        var result = SocialMediaChecker.check('https://www.facebook.com/john');
         expect(result).toEqual('facebook');
     });
 });
 
 describe('Google+', () => {
 
-    it('returns google"+" y for google+ url', () => {
-        var result = checker.check('https://www.plus.google.com/john');
+    it('returns "google+" for google+ url', () => {
+        var result = SocialMediaChecker.check('https://www.plus.google.com/john');
         expect(result).toEqual('google+');
     });
 });
 
 describe('Instagram', () => {
 
-    it('returns "instagram" y for facebook url', () => {
-        var result = checker.check('https://www.instagram.com/john');
+    it('returns "instagram" for instagram url', () => {
+        var result = SocialMediaChecker.check('https://www.instagram.com/john');
         expect(result).toEqual('instagram');
     });
 });
 
 describe('Linkedin', () => {
 
-    it('returns "linkedin" y for linkedin url', () => {
-        var result = checker.check('https://www.linkedin.com/john');
+    it('returns "linkedin" for linkedin url', () => {
+        var result = SocialMediaChecker.check('https://www.linkedin.com/john');
         expect(result).toEqual('linkedin');
     });
 });
